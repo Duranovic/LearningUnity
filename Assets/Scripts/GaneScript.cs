3d681a4b46f82e04be07b0786ac91dd4817351e7
Velid Duranovic <velid_work@outlook.com>
2020-08-05 16:49:38 +0200

    Added opponent answer and options for user to select

--- Assets/Scripts/GaneScript.cs
+++ Assets/Scripts/GaneScript.cs
@@ -4,21 +4,45 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public class GaneScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public new GameObject gameObject;
-    public Animator animator;
+    private int _opponentAnswer;
     void Start()
     {
-        gameObject.GetComponentInChildren<Text>().text = DateTime.UtcNow.ToShortDateString();
-    }
-    void Update()
+    }    
+    void generateOpponentAnswer()
     {
-        if (Input.touches.Any(x => x.phase == TouchPhase.Began))
+        Random random = new Random();
+        _opponentAnswer = random.Next(1, 5);
+    }
+    static public GameObject getChildGameObject(GameObject fromGameObject, string withName)
+    {
+        //Author: Isaac Dart, June-13.
+        Transform[] ts = fromGameObject.transform.GetComponentsInChildren<Transform>(true);
+        foreach (Transform t in ts) if (t.gameObject.name == withName) return t.gameObject;
+        return null;
+    }
+    public void ButtonClicked()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        generateOpponentAnswer();
+        gameObject.GetComponentInChildren<Text>().text = _opponentAnswer.ToString();
+        //getChildGameObject(gameObject, "sprite").GetComponent<Animator>().SetBool("isGameStarted", true);
+        if (getChildGameObject(gameObject, "Sprite") == null)
         {
-            animator.SetBool("isGameStarted", true);
+            Debug.Log("NULLLLL");
+        }
+        else
+        {
+            getChildGameObject(gameObject, "Sprite").GetComponent<Animator>().SetBool("isGameStarted", true);
         }
     }
 }
