16aaa29e7419631c85294b5da99e83e6055380aa
Velid Duranovic <velid_work@outlook.com>
2020-08-05 14:20:43 +0200

    Added animated hand

--- Assets/Scripts/GaneScript.cs
+++ Assets/Scripts/GaneScript.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +9,16 @@ public class GaneScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public new GameObject gameObject;
+    public Animator animator;
     void Start()
     {
         gameObject.GetComponentInChildren<Text>().text = DateTime.UtcNow.ToShortDateString();
+    }
+    void Update()
+    {
+        if (Input.touches.Any(x => x.phase == TouchPhase.Began))
+        {
+            animator.SetBool("isGameStarted", true);
+        }
     }
 }
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,12 +6,6 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public Animator animator;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-        
-    }
 
     // Update is called once per frame
     private void Update()
